51a99e61f3b83cc6a77fb079297ae36ce689041e
xie <1312923196@qq.com>
2023-07-17 22:06:52 +0800

    修复openapi页面未指定版本问题

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.openapi.docs import (
     get_swagger_ui_html,
     get_swagger_ui_oauth2_redirect_html,
 )
+from fastapi.openapi.utils import get_openapi
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 
@@ -70,6 +71,25 @@ async def redoc_html():
         redoc_js_url="/static/redoc.standalone.js",
     )
 
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        openapi_version='3.0.2',
+        summary="This is a very custom OpenAPI schema",
+        description="Here's a longer description of the custom **OpenAPI** schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app', reload=True,workers=3)
