1542c73dd69206819a1d747dadc2f8d5ad1fcd40
xie <1312923196@qq.com>
2024-11-03 22:25:22 +0800

    wer

--- src/api/v1/dk_data_source_api.py
+++ src/api/v1/dk_data_source_api.py
@@ -9,7 +9,7 @@
 from dependency_injector.wiring import Provide, inject
 from fastapi import Depends, Query
 from fastapi_pagination import Page, pagination_ctx
-
+from src.core.dependency import CustomPage
 from src.common import RestGet, VControllerBase
 from src.core import ResultJson
 from src.core.error import NotFoundError
@@ -23,24 +23,22 @@ class DkDnsRouter(VControllerBase):
     """
     prefix = "dataSourcesInfo"
     tags = ["数据源"]
-    Page = Page.with_custom_options(size=Query(10, ge=1, le=100, description="Page size limit"))
+    page = CustomPage
     response_schema = DkDataSourcesSchema
     dns_service = Provide[DkDnsContainer.service]
     logger = Provide[DkDnsContainer.logger]
 
-    @RestGet(
-        path='/getDataSources',
-        summary='获取数据源',
-        dependencies=[Depends(pagination_ctx(Page))],
-        response_model=ResultJson[Page[response_schema]])
-    def ov_get_all(self):
-        result = self.dns_service.get_all()
+    @RestGet(path='/getDataSources',
+             summary='获取数据源',
+             dependencies=[Depends(pagination_ctx(page))],
+             response_model=ResultJson[page[response_schema]])
+    async def ov_get_all(self):
+        result = await self.dns_service.get_all()
         return ResultJson(data=result)
 
-    @RestGet(
-        path='/getDataSourceInfo/{dns_id}',
-        summary="数据源详情",
-        response_model=ResultJson[response_schema])
+    @RestGet(path='/getDataSourceInfo/{dns_id}',
+             summary="数据源详情",
+             response_model=ResultJson[response_schema])
     async def ov_get_one(self, dns_id: str):
         try:
             result = await self.dns_service.get_one(dns_id)
