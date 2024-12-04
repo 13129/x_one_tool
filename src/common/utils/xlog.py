09943d5fd10a1009878df848dd01ccae78b4f41f
xiejch <1312923196@qq.com>
2024-12-04 18:31:47 +0800

    调整20241204

---
+++ src/common/utils/xlog.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@FileName  :xlogging.py
+@Time      :2024/12/4 16:52
+@Author    :XJC
+@Description:
+"""
+
+# from src.core.container import BaseContainer
+#
+#
+# class XLogMixin:
+#     logger = BaseContainer.logger().xlog
+#
+#     @property
+#     def log(self):
+#         return self.logger
